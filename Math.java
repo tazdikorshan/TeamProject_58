4d5d2831f4240743f4f965c23a8b67c81cff9006
Tazdik Wazi Orshan <116716047+tazdikorshan@users.noreply.github.com>
2025-10-21 15:20:37 +0100

    changed the file name to avoid conflicts

--- Math.java
+++
@@ -1,9 +0,0 @@
-public class Math {
-    int Max(int a, int b) {
-        if (a > b) {
-            return a;
-        } else {
-            return b;
-        }
-    }
-}
---
+++ Tazdik_Math.java
@@ -0,0 +1,16 @@
+/********************************
+ * Developer: Tazdik Wazi Orshan
+ * University ID: 240350255
+ * Function: This function takes two inputs as integers and returns the maximum
+ * number
+ ********************************/
+
+public class Tazdik_Math {
+    int Max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+}
