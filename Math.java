dfbdce250c95aaecdea7d4ce38888ccb59bc6257
hoonathan521-commits <hoonathan521@gmail.com>
2025-10-21 13:55:55 +0100

    Nathan java divide method added

---
+++ Math.java
@@ -0,0 +1,7 @@
+public class Math {
+    //Divide method which returns a whole integer
+    private int divide(int num1, int num2){
+        int result = num1 / num2; 
+        return result; 
+    }
+}
