4d6b934108eb5a33e7e3a12416f4f85a5a950556
amanjit805 <amanjit805@gmail.com>
2026-03-07 18:20:14 +0000

    Created ChatbotController for my chatbot
    I needed to make this because only working on javascript makes the chatbot limited and its better if i have access to the database to a certain extent

---
+++ app/Http/Controllers/ChatbotController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ChatbotController extends Controller
+{
+    public function handle(Request $request)
+    {
+        $message = strtolower($request->message);
+        $products = Product::all();
+        foreach ($products as $product) {
+            $productName = strtolower($product->name);
+            if (str_contains($message, $productName)) {
+                return response()->json([
+                    'reply' => "{$product->name} costs £{$product->price} and we have {$product->stock_quantity} in stock."
+                    ]);
+            }
+        }
+        if (str_contains($message, 'products')) {
+            $reply = "Here are some of our products:";
+            foreach ($products as $product) {
+                $reply .= "{$product->name} - £{$product->price} ({$product->stock_quantity} available)";
+            }
+            return response()->json(['reply' => $reply]);
+        }
+        if (str_contains($message, 'stock')) {
+            foreach ($products as $product) {
+                if (str_contains($message, strtolower($product->name))) {
+                    return response()->json([
+                        'reply' => "We currently have {$product->stock_quantity} {$product->name}(s) available."
+                    ]);
+                }
+            }
+        }
+        if (str_contains($message, 'price') || str_contains($message, 'cost')) {
+            foreach ($products as $product) {
+                if (str_contains($message, strtolower($product->name))) {
+                    return response()->json([
+                        'reply' => "The price of {$product->name} is £{$product->price}."
+                    ]);
+                }
+            }
+        }
+        return response()->json([
+            'reply' => "Sorry, I didn't understand that. I can solve any queries regarding products, price, stock or orders. "
+        ]);
+    }
+}
