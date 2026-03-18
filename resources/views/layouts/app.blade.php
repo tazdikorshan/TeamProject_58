9733fcc7385d80cddfc3cbedd0cdc7527ae82d8a
amanjit805 <amanjit805@gmail.com>
2026-03-18 10:41:32 +0000

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -659,6 +659,105 @@
         .footer-col a:hover {
             text-decoration: underline;
         }
+        .open-button {
+  background: var(--hd-orange);
+  cursor: pointer;
+  border-color: brown;
+  width: 65px;
+  height: 65px;
+  border-radius: 60px;
+  position: fixed;
+  bottom: 13px;
+  right: 13px;
+  justify-content: center;
+  font-size: 11px;
+}
+
+.open-button img {
+  width: 60px;
+  height: 60px;
+}
+
+.chatbot {
+  display: none;
+  position: fixed;
+  bottom: 8px;
+  right: 8px;
+}
+
+.chatbot-container {
+  width: 300px;
+  height: 420px;
+  border-radius: 13px;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+.chatbot-title {
+  background: var(--hd-orange);
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: bold;
+  padding: 15px;
+}
+
+.chatbot-title button {
+  background: none;
+  border: none;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.chatbot-conversation {
+  flex: 1;
+  padding: 10px;
+  overflow-y: auto;
+  background:rgb(243, 243, 243);
+}
+
+.message {
+  padding: 10px;
+  margin: 5px 0;
+  border-radius: 10px;
+  max-width: 80%;
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+  word-break: break-word;
+}
+
+.bot {
+  background:#ddd;
+  color: black;
+}
+
+.user {
+  background: var(--hd-orange);
+  color: white;
+  margin-left: auto;
+}
+
+.chatbot-send {
+  display: flex;
+}
+
+.chatbot-send input {
+  flex: 1;
+  padding: 15px;
+  border: none;
+  background: #d2d2d2;
+}
+
+.chatbot-send button {
+  background-color: var(--hd-orange);
+  color: white;
+  border: none;
+  padding: 0 16px;
+  cursor: pointer;
+}
     </style>
 </head>
 
@@ -670,6 +769,8 @@
     </main>
 
     @include('partials.footer')
+
+    @include('paritals.Chatbot')
 </body>
 
 </html>
