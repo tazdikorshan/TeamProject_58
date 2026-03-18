363a7a17ea584b7c52773d2655b5c5d50300d034
amanjit805 <amanjit805@gmail.com>
2026-03-18 02:46:41 +0000

    Added missing pages from the footer and added routes in the web.php for my chatbot and the pages of the footer

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\FilterController;
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\PastOrderController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\ChatbotController;
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/wishlist', [WishlistController::class, 'index'])->name('wishlist.index');
@@ -124,9 +125,18 @@ Route::get('/Shipping-options', function () {
     return view('Shipping-options');
 })->name('Shipping-options');
 
-Route::get('/Track-order', function () {
-    return view('Track-order');
-})->name('Track-order');
+Route::get('/TrackOrder', function () {
+    return view('TrackOrder');
+})->name('TrackOrder');
+
+Route::get('/Terms&Conditions', function () {
+    return view('Terms&Conditions');
+})->name('Terms&Conditions');
+
+Route::get('/Privacy-policy', function () {
+    return view('Privacy-policy');
+})->name('Privacy-policy');
+
 Route::post('/submit-review', [FeedbackController::class, 'store']);
 Route::get('/search', [SearchController::class, 'search'])->name('search');
 Route::get('/filter', [FilterController::class, 'filter'])->name('filter');
@@ -138,3 +148,5 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/admin/customers/{id}/update', [AdminController::class, 'customersUpdate']);
     Route::post('/admin/customers/{id}/delete', [AdminController::class, 'customersDelete']);
 });
+
+Route::post('/chatbot', [ChatbotController::class, 'handle'])->name('chatbot.handle');
